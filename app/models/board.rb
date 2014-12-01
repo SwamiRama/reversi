246e2e941ca2f015dd2ba52a034f941f87008393
Jimmi <gitlab@rl-hosting.de>
2014-12-01 03:12:30 +0100

    add some new methods to reversi.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -11,8 +11,8 @@ class Board
     @tile_positions << {:tile => stone_player_two, :row => SIZE / 2 - 1, :col => SIZE / 2}
   end
 
-  def set_tile(row, col, new_tile)
-    if row < SIZE && col < SIZE && row >= 0 && col >= 0
+  def set_tile_allowed?(row, col, new_tile)
+    if on_board?(row, col)
       good_move = true 
       @tile_positions.each do |tile|
         if tile[:row] == row && tile[:col] == col
@@ -21,12 +21,48 @@ class Board
         end
       end
       if good_move
-        @tile_positions << {:tile => new_tile, :row => row, :col => col}
+        return true
       end
     else
-      puts "move out of size!"
+      return false
     end
   end
+
+  def change_tile(row, col, current_player)
+    if get_slot(row, col) != current_player
+      @tile_positions.each do |tile|
+        if tile[:row] == row && tile[:col] == col
+          tile = {:tile => current_player, :row => row, :col => col}
+        end
+      end
+    end
+  end
+
+  def set_tile(row, col, current_player)
+    @tile_positions.each do |tile|
+      if tile[:row] == row && tile[:col] == col
+        tile = {:tile => current_player, :row => row, :col => col}
+      end
+    end
+  end
+
+
+  def on_board?(row, col)
+    if row < SIZE && col < SIZE && row >= 0 && col >= 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_tile(row, col)
+    @tile_positions.each do |tile|
+      if tile[:row] == row && tile[:col] == col
+        tile[:tile]
+      end
+    end
+  end
+
 
   def draw
     0.upto(SIZE - 1) { |row|
--- app/models/reversi.rb
+++ app/models/reversi.rb
@@ -5,6 +5,7 @@ class Reversi
   @nextTurn
   @board
   @level
+  @current_player
   @BOARD_IMPORTANCE = [
     [9999, 5, 500, 200, 200, 500, 5, 9999],
     [5, 1, 50, 150, 150, 50, 1, 5],
@@ -17,27 +18,92 @@ class Reversi
   ]
   
   
-  def initialize(firstTurn, level)
-    if firstTurn == nil
-      raise ArgumentError, "firstTurn is nil"
+  def initialize(player_first, level)
+    if player_first == nil
+      raise ArgumentError, "player_first is nil"
     end
-    @board = Board.new('X', 'O')
     @current_player = 'X'
+    @board = Board.new(@current_player, 'O')
   end
 
   def move(row, col)
-    if @board.set_tile(row, col, @current_player)
-      -1.upto(1) { |row_direction|  end
-        -1.upto(1) { |col_direction|
-          puts row_direction
-          puts col_direction
-        }
-      }
+    if game_over?
+      puts 'game over!'
+    elsif is_move_allowed(row, col)
+      set_tile(row, col)
+      return true
+    else
+      return false
+    end
+  end
+
+  def next_player
+    if @current_player == 'O'
+      @current_player = 'X'
+    else
+      @current_player = 'O'
     end
   end
 
   private
-  def legal_move?(row, col)
-    
+  def game_over?
+    return false
+  end
+
+  def set_tile(row, col)
+    @board.set_tile(row, col, @current_player)
+    -1.upto(1) { |row_direction|
+      -1.upto(1) { |col_direction|
+        if row_direction != 0 || col_direction != 0
+          adjacent_stones = get_adjacent_stones(row, col, row_direction, col_direction)
+          puts "start set_tile loop"
+          while adjacent_stones.empty?            
+            position = adjacent_stones.pop
+            @board.set_tile(position[:row], position[:col], @current_player)
+          end
+        end
+      }
+    }
+  end
+
+  def is_move_allowed(row, col)
+    if @board.set_tile_allowed?(row, col, @current_player)
+      -1.upto(1) { |row_direction|
+        -1.upto(1) { |col_direction|
+          if row_direction != 0 || col_direction != 0
+            adjacent_stones = get_adjacent_stones(row, col, row_direction, col_direction)
+            if !adjacent_stones.nil? && !adjacent_stones.empty?
+              return true
+            end
+          end
+        }
+      }
+    end
+    return false
+  end
+
+  def get_adjacent_stones(row, col, row_direction, col_direction)
+    if row_direction == 0 && col_direction == 0
+      raise "there is no direction"
+    end
+
+    check_row = row + row_direction
+    check_col = col + col_direction
+
+    fields = []
+    puts "start get_adjacent_stones loop"
+    while @board.on_board?(check_row, check_col)
+      slot = @board.get_tile(check_row, check_col)
+      if @current_player == slot
+        return fields
+      elsif slot.nil?
+        return nil
+      else
+        slot << {:row => check_row, :col => check_col}
+      end
+      check_row = check_row + row_direction
+      check_col = check_col * col_direction
+    end
+    return nil    
   end
 end
