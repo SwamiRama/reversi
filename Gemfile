030ad8f962581080b4aa726b7f02f660f908ecd5
Manuel Reischl <mreischl@crealytics.de>
2014-12-02 17:21:11 +0100

    add ai.rb and update Gemfile with shoes

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'interface', '~> 1.0.3'
 gem 'require_all'
+gem 'green_shoes'
+
---
+++ app/models/ai.rb
@@ -0,0 +1,9 @@
+class Ai
+  @ai_token
+  def initialize(token)
+    @ai_token = token
+  end
+
+  def move
+  end
+end
