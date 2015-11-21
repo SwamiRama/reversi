f55ab194f347fe050644a014e8a9d8c04aa99b56
Manuel <manuel@rl-hosting.de>
2015-11-21 22:41:32 +0100

    refactoring

--- spec/app/models/ai/ai_spec.rb
+++ spec/app/models/ai/ai_spec.rb
@@ -1,72 +1,71 @@
 require 'spec_helper'
 
 describe 'Ai' do
-  before(:all) do
-    @reversi = Reversi.new
-  end
+  let(:reversi) { Reversi.new }
+
   context 'move' do
     describe 'first move' do
       it 'should move' do
-        expect(Ai.best_next_move(@reversi, 5)).to eq ({ row: 2, col: 4 })
+        expect(Ai.best_next_move(reversi, 5)).to eq ({ row: 2, col: 4 })
       end
     end
     describe 'second move' do
       it 'should move' do
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(@reversi.ai_move).to eq true
-        expect(Ai.best_next_move(@reversi, 1)).to eq ({ row: 7, col: 1 })
-        expect(@reversi.ai_move).to eq true
-        expect(Ai.best_next_move(@reversi, 1)).not_to eq nil
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(reversi.ai_move).to eq true
+        expect(Ai.best_next_move(reversi, 1)).to eq ({ row: 7, col: 1 })
+        expect(reversi.ai_move).to eq true
+        expect(Ai.best_next_move(reversi, 1)).not_to eq nil
       end
     end
   end
