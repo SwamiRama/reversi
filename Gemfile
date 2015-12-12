161489d608fc1807dd3d4a42f8668b9ca49cfe7d
Manuel Reischl <Jimmi@jimmi.speedport.ip>
2015-12-12 18:40:22 +0100

    add green_shoes view

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'require_all'
+gem 'green_shoes', '~> 1.1', '>= 1.1.374'
+
 group :development do
   gem 'rspec'
   gem 'pry'
--- main.rb
+++ main.rb
@@ -1,45 +1,46 @@
 require_relative 'app/models/reversi'
 require 'pry'
+require 'green_shoes'
 
+TITLE = 'Reversi'
+WIDTH = 600
+HEIGHT = 600
+TILE_SIZE = 30
 
+Shoes.app title: TITLE, width: WIDTH, height: HEIGHT do
+  def tile(x, y, color)
+    fill color
+    oval(x, y, TILE_SIZE / 2)
+  end
 
-def input_handler(input)
-  case input
-  when 'move', 'm'
-    puts 'row:'
-    row = gets
-    puts 'col'
-    col = gets
-    if @reversi.move(row.to_i, col.to_i)
-      @reversi.ai_move
+  def player_tile(x, y, stone)
+    case stone
+    when @reversi.player.one
+      tile x, y, black
+    when @reversi.player.two
+      tile x, y, white
     end
-  when 'help', 'h'
-    puts 'do help'
-  when 'draw', 'd'
-    @reversi.draw
-  when 'quit', 'exit', 'q'
-    @quit = true
-  else
-    puts 'ERROR: Wrong input'
+  end
+
+  @reversi = Reversi.new
+
+  animate = animate 1 do
+    player_tile WIDTH - TILE_SIZE, 0, @reversi.player.current
+
+    @reversi.board.board.each_with_index do |array, index_x|
+      array.each_with_index do |stone, index_y|
+        draw_x = index_x * TILE_SIZE
+        draw_y = index_y * TILE_SIZE
+
+        fill green
+        rect(draw_x, draw_y, TILE_SIZE).click { @reversi.move index_x, index_y }
+        player_tile(draw_x, draw_y, stone)
+      end
+    end
+    @reversi.ai_move
+    if @reversi.game_over?
+      alert 'Game Over'
+      animate.stop
+    end
   end
 end
-
-pp 'Welcome to Reversi'
-@reversi = Reversi.new
-while(!@reversi.game_over? && !@quit)
-  pp 'current Player = ' + @reversi.current_player
-  pp 'm or move'
-  pp 'd or draw'
-  pp 'h or help'
-  input = gets.delete("\n")
-  input_handler(input)
-end
-
-
-##############################################
-
-#Shoes.app do
-#  @reversi = Reversi.new
-#  @board_view = BoardView.new(@reversi, self)
-#  @board_view.draw
-#end
